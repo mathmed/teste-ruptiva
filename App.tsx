7f4f723231c9736a52ccf532c2e14a00a5f8af8d
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2020-12-02 22:00:50 -0300

    config: adiciona configuração inicial

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RegisterScreen from './src/app/screens/Register';
+
+const App: React.FC = () => <RegisterScreen />;
+
+export default App;
---
+++ src/app/screens/Register/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const RegisterScreen: React.FC = () => <Text>Olá Mundo</Text>;
+
+export default RegisterScreen;
